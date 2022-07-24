6620557f874d84a7a292fbb5e72150d1e3b73e24
iwasawa15 <tatsuya57sak@gmail.com>
2022-07-25 03:28:17 +0900

    [fix]: redirectを無効

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const withImages = require('next-images');
 const redirects = {
   async redirects() {
     return [
-      {
-        source: '/dashboards',
-        destination: '/dashboards/crypto',
-        permanent: true
-      }
     ];
   }
 };
